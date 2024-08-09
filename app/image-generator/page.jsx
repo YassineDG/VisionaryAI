7a8fd9c21627fbe853d1cf6a5de0f6c7a9b46d34
Yassine Dorgâa <58447334+YassineDG@users.noreply.github.com>
2024-08-09 18:53:18 +0100

    Update page.jsx

--- app/image-generator/page.jsx
+++ app/image-generator/page.jsx
@@ -28,7 +28,7 @@ import Footer from "@/components/footer";
 export default function ImageGeneratorPage() {
   const [prompt, setPrompt] = useState("");
   const [promptError, setPromptError] = useState("");
-  const [selectedModel, setSelectedModel] = useState("playground-v2");
+  const [selectedModel, setSelectedModel] = useState("flux-schnell");
   const [selectedAspectRatio, setSelectedAspectRatio] = useState("1:1");
   const [imageUrl, setImageUrl] = useState("/placeholder.svg");
   const [isLoading, setIsLoading] = useState(false);
@@ -175,14 +175,14 @@ export default function ImageGeneratorPage() {
                 value={selectedModel}
                 onValueChange={setSelectedModel}
               >
-                <DropdownMenuRadioItem value="playground-v2">
-                  Playground V2 (Default)
+                <DropdownMenuRadioItem value="flux-schnell">
+                flux-schnell (Default)
                 </DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value="kandinsky-2.2">
-                  Kandinsky 2.2
+                <DropdownMenuRadioItem value="stable-diffusion-3">
+                stable-diffusion-3
                 </DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value="dreamshaper-xl-turbo">
-                  Dreamshaper XL Turbo
+                <DropdownMenuRadioItem value="sdxl-lightning">
+                sdxl-lightning
                 </DropdownMenuRadioItem>
               </DropdownMenuRadioGroup>
             </DropdownMenuContent>
